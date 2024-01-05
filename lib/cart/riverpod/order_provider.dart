5ddbbfe817ea620a0f63a76635c28e69f955207e
Gaurang Choudhary <gaurang.61084135@ltimindtree.com>
2024-01-05 10:38:12 +0530

    orders displayed after payment completion

---
+++ lib/cart/riverpod/order_provider.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class Order extends StateNotifier<List> {
+  Order() : super([]);
+  void add(data) {
+    state = [...state, ...data];
+  }
+}
+
+final OrderProvider = StateNotifierProvider<Order, List>((ref) => Order());
